988c783e0a07c301c5c29f2d3f843888c2116484
Liren Tu <tuliren@gmail.com>
2024-11-30 15:40:50 -0800

    Replace tabs with toolbar

--- src/components/app/ContentCard.tsx
+++ src/components/app/ContentCard.tsx
@@ -3,39 +3,29 @@ import BaseCard from '@/components/app/BaseCard';
 import CustomMarkdown from '@/components/app/CustomMarkdown';
 import { FormDataMap } from '@/params/common';
 import { processMarkdown } from '@/params/contentHelpers';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import PdfViewer from '@/components/app/PdfViewer';
 
 interface ContentCardProps {
   title: string;
   content: string;
   formData: FormDataMap;
+  view?: 'text' | 'pdf';
 }
 
-const ContentCard: FC<ContentCardProps> = ({ title, content, formData }) => {
+const ContentCard: FC<ContentCardProps> = ({ title, content, formData, view = 'text' }) => {
   const processedContent = processMarkdown(content, formData);
   const contentRef = useRef<HTMLDivElement>(null);
 
   return (
     <BaseCard title={title}>
-      <Tabs defaultValue="text" className="w-full flex flex-col gap-2">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="text">Text</TabsTrigger>
-          <TabsTrigger value="pdf">PDF</TabsTrigger>
-        </TabsList>
-
-        <TabsContent value="text">
-          <CustomMarkdown
-            containerRef={contentRef}
-            content={processedContent}
-            textClassNames={['text-sm', 'text-gray-700']}
-          />
-        </TabsContent>
-
-        <TabsContent value="pdf">
-          <PdfViewer content={processedContent} />
-        </TabsContent>
-      </Tabs>
+      {view === 'text' && (
+        <CustomMarkdown
+          containerRef={contentRef}
+          content={processedContent}
+          textClassNames={['text-sm', 'text-gray-700']}
+        />
+      )}
+      {view === 'pdf' && <PdfViewer content={processedContent} />}
     </BaseCard>
   );
 };
